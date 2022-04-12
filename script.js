0dc1ab279d4db70fabd4b3d5a869b13c01d9ab22
Nicole Antunes <102484958+nicoleantuness@users.noreply.github.com>
2022-04-12 17:21:00 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ function positions(firstPlace, secondPlace, lastPlace){
         console.log(`2° colocado ${podio[0]}`)
         console.log(`3° colocado ${podio[2]}`)
     }else if(podio[2] == "Daniel"){
-        console.log(`1° colocado ${podio[0]}`)
-        console.log(`2° colocado ${podio[2]}`)
-        console.log(`3° colocado ${podio[1]}`)
+        console.log(`1° colocado ${podio[2]}`)
+        console.log(`2° colocado ${podio[1]}`)
+        console.log(`3° colocado ${podio[0]}`)
     }
 }else{
    console.log(`1° colocado ${podio[0]}`)
@@ -17,32 +17,6 @@ function positions(firstPlace, secondPlace, lastPlace){
    console.log(`3° colocado ${podio[2]}`)
 }
 }
-console.log(positions("Manoel", "Daniel","Rafael"))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* if(podio[0] += podio[1]){
-       return podio[0]
-   }
-   if(podio[0] += podio[2]){
-     return podio[0]  
-   }
-    let resultado = arr1.concat(podio)
-    console.log(resultado) */
+console.log(positions("Rafael", "Manoel","Daniel"))
+console.log(positions("Manoel", "Daniel", "Rafael"))
+console.log(positions("Daniel", "Rafael", "Manoel"))
